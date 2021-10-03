88d1faedc6547653f6413d55a79d9383e68f7ff7
Konstantin Gvencadze <humblebumble17@gmail.com>
2021-10-03 10:49:58 +0300

    Linter fixes (#16)
    * fix(linter): fix all linter warnings and disable errcheck linter.
    
    * fix(client): add error handle to gzip.NewReader()
    
    * fix(client): add return in switch...case

--- cli/completer/completer.go
+++ cli/completer/completer.go
@@ -2,14 +2,16 @@ package completer
 
 import "github.com/c-bata/go-prompt"
 
+// Completer object
 type Completer struct {
-
 }
 
+// New - returns completer object
 func New() *Completer {
 	return &Completer{}
 }
 
+// Complete - returns suggestions for input
 func (c *Completer) Complete(d prompt.Document) []prompt.Suggest {
 	return []prompt.Suggest{}
 }
--- pkg/clickhouse/client.go
+++ pkg/clickhouse/client.go
@@ -2,11 +2,13 @@ package clickhouse
 
 import "context"
 
+// Response of clickhouse
 type Response struct {
 	QueryID string
-	Data string
+	Data    string
 }
 
+// Client - interface implementing Clickhouse
 type Client interface {
 	Query(ctx context.Context, query string) (string, error)
 }
